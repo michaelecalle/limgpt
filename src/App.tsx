3bb76e963e62761dc715f53703de4791eaffe450
michaelecalle <michael@radioequinoxe.com>
2025-11-14 12:56:14 +0100

    GPS Lab

--- src/App.tsx
+++ src/App.tsx
@@ -402,3 +402,10 @@ export default function App() {
 
           {/* Bloc FT */}
           <div className="mt-3 flex-1 min-h-0">
+            <FT />
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
